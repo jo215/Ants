eb87f8e465ae600970af613a5040fe099face4c9
James OHara <jo215@sussex.ac.uk>
2012-05-15 16:29:38 +0100

    Refactored anonymous listener to own private class for clarity.

--- Antz/src/ui/GameplayScreen.java
+++ Antz/src/ui/GameplayScreen.java
@@ -8,10 +8,12 @@ import java.awt.Rectangle;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EtchedBorder;
 
 import program.GameManager;
 import world.World;
@@ -21,6 +23,8 @@ public class GameplayScreen extends JFrame {
 
 
 	private MapPanel mapPanel;
+	private ScorePanel scorePanel;
+	
 	private final World world;
 	
 	/**
@@ -30,58 +34,27 @@ public class GameplayScreen extends JFrame {
 	public GameplayScreen(final World world)
 	{
 		super("Antz");
-
 		this.world = world;
-		
 	    Container pane = this.getContentPane();
+	    JPanel outerPanel = new JPanel(new BorderLayout());
+	    pane.add(outerPanel);
 	    JPanel mainPanel = new JPanel(new BorderLayout());
-	    pane.add(mainPanel);	
-	    mainPanel.add("Center", new JPanel());
-	    mainPanel.add("North", new JPanel());
+	    outerPanel.add("Center", mainPanel);
+	    
+	    //	The main map panel, within a JScrollPane
+		mapPanel = new MapPanel(this, world);
+		JScrollPane scrollPane = new JScrollPane(mapPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollPane.setWheelScrollingEnabled(false);
+		scrollPane.setDoubleBuffered(true);
+		scrollPane.addMouseWheelListener(new MapScrollListener(scrollPane));
+		mainPanel.add("Center", scrollPane);
+		JPanel temp = new JPanel();
+		scorePanel = new ScorePanel(this, world);
+		temp.add(scorePanel);
+	    mainPanel.add("North", temp);
 	    mainPanel.add("South", new JPanel());
 	    mainPanel.add("East", new JPanel());
 	    mainPanel.add("West", new JPanel());
-	    
-		mapPanel = new MapPanel(this, world);
-		final JScrollPane scrollPane = new JScrollPane(mapPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-
-		scrollPane.setWheelScrollingEnabled(false);
-		scrollPane.setDoubleBuffered(true);
-
-		scrollPane.addMouseWheelListener(new MouseWheelListener() {
-			/**
-			 * Scroll Map functionality
-			 */
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				// Save the previous coordinates
-				int oldZoom = mapPanel.getZoomLevel();
-				Rectangle oldView = scrollPane.getViewport().getViewRect();
-
-				// resize the panel for the new zoom
-				int wheel = e.getWheelRotation();
-				mapPanel.setZoomLevel(mapPanel.getZoomLevel() + wheel);
-				if (mapPanel.getZoomLevel() < 1) {
-					mapPanel.setZoomLevel(1);
-				} else if (mapPanel.getZoomLevel() > 2) {
-					if (wheel > 0)
-						mapPanel.setZoomLevel(4);
-					else
-						mapPanel.setZoomLevel(2);
-				}
-				// calculate the new view position - this isn't perfect
-				mapPanel.setPreferredSize(new Dimension(world.getWidth() * MapPanel.imageWidth / mapPanel.getZoomLevel(), (int)(world.getHeight() * MapPanel.imageHeight / mapPanel.getZoomLevel() / 1.333)));
-				int newZoom = mapPanel.getZoomLevel();
-				Point newViewPos = new Point();
-
-				newViewPos.x = Math.max(0, (oldView.x + oldView.width / 2) * oldZoom / newZoom - oldView.width / 2);
-				newViewPos.y = Math.max(0, (oldView.y + oldView.height / 2) * oldZoom / newZoom - oldView.height / 2);
-				
-				scrollPane.getViewport().setViewPosition(newViewPos);
-				mapPanel.revalidate();
-				
-			}
-		});
-		mainPanel.add("Center", scrollPane);
 	    
 	    //  final initialization
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -97,5 +70,54 @@ public class GameplayScreen extends JFrame {
 	public void update()
 	{
 		mapPanel.repaint();
+		scorePanel.redScore.setText("Score: " + world.getRedScore());
+		scorePanel.blackScore.setText("Score: " + world.getBlackScore());
+		scorePanel.scoreRatio.repaint();
+	}
+	
+	/**
+	 * Scroll Pane listener (for mouse wheel zoom);
+	 * @author JOH
+	 * @version 0.1
+	 */
+	private class MapScrollListener implements MouseWheelListener {
+		
+		private JScrollPane scrollPane;
+		
+		/**
+		 * Constructor.
+		 * @param scrollPane
+		 */
+		public MapScrollListener(JScrollPane scrollPane) {
+			this.scrollPane = scrollPane;
+		}
+
+		@Override
+		public void mouseWheelMoved(MouseWheelEvent e) {
+			// Save the previous coordinates
+			int oldZoom = mapPanel.getZoomLevel();
+			Rectangle oldView = scrollPane.getViewport().getViewRect();
+
+			// resize the panel for the new zoom
+			int wheel = e.getWheelRotation();
+			mapPanel.setZoomLevel(mapPanel.getZoomLevel() + wheel);
+			if (mapPanel.getZoomLevel() < 1) {
+				mapPanel.setZoomLevel(1);
+			} else if (mapPanel.getZoomLevel() > 2) {
+				if (wheel > 0)
+					mapPanel.setZoomLevel(4);
+				else
+					mapPanel.setZoomLevel(2);
+			}
+			// calculate the new view position - this isn't perfect
+			mapPanel.setPreferredSize(new Dimension(world.getWidth() * MapPanel.imageWidth / mapPanel.getZoomLevel(), (int)(world.getHeight() * MapPanel.imageHeight / mapPanel.getZoomLevel() / 1.333)));
+			int newZoom = mapPanel.getZoomLevel();
+			Point newViewPos = new Point();
+			newViewPos.x = Math.max(0, (oldView.x + oldView.width / 2) * oldZoom / newZoom - oldView.width / 2);
+			newViewPos.y = Math.max(0, (oldView.y + oldView.height / 2) * oldZoom / newZoom - oldView.height / 2);
+			scrollPane.getViewport().setViewPosition(newViewPos);
+			//	Redraw the panel
+			mapPanel.revalidate();	
+		}
 	}
 }
