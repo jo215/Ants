656dc3016c9bf12d322f99340a2692da8c398496
James OHara <jo215@sussex.ac.uk>
2012-05-20 23:44:20 +0100

    Now sets the random seed for testing.

--- Antz/src/program/Main.java
+++ Antz/src/program/Main.java
@@ -20,8 +20,12 @@ public class Main {
 	public static void main(String[] args)
 	{
 		setNimbusLookAndFeel();
+		//	Remember to set the seed
+		RandGenerator.setSeed(12345);
 		GameManager gm = new GameManager();
 		
+		//for (int i =0; i <100; i++)
+			//System.out.println("" + RandGenerator.randomInt(16384));
 	}
 
 	/**
